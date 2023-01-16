50370ea9e31c975b8ab8a3f21cf069c0cc74061e
Alex <ais_alex@ukr.net>
2023-01-16 05:40:45 +0200

    test 2

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { FrindListItem } from './FrindListItem';
 
 export function FriendList({ friends }) {
--- src/components/FriendList/FrindListItem.jsx
+++ src/components/FriendList/FrindListItem.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 export function FrindListItem({ id, avatar, name, isOnline }) {
   return (
